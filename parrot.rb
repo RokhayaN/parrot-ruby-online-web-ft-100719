3362f918071f75f9757b2f2f92760d51e3a91a9c
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-09 03:16:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 def parrot (animal,sound = "squack")
   animal = "parrot"
   puts "A #{animal} make a #{sound}"
+  parrot
 end 
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
